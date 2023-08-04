030fb51392c0baa104a99a7508c9136fec27de02
viking <viking@vikingdeMacBook-Pro.local>
2023-08-04 17:54:00 +0800

    fix: auto update remote plugins

--- lua/plugins/_wilder.lua
+++ lua/plugins/_wilder.lua
@@ -1,38 +1,39 @@
 local M = {}
 local spec = {
-  "gelguy/wilder.nvim",
-  event = "VeryLazy",
-  config = function()
-    local wilder = require("wilder")
-    wilder.setup({
-      modes = { ":", "/", "?" },
-      next_key = "<C-j>",
-      previous_key = "<C-k>",
-    })
-    wilder.set_option(
-      "renderer",
-      wilder.renderer_mux({
-        [":"] = wilder.popupmenu_renderer({
-          highlighter = wilder.basic_highlighter(),
-        }),
-        ["/"] = wilder.wildmenu_renderer({
-          highlighter = wilder.basic_highlighter(),
-        }),
-      })
-    )
+	"gelguy/wilder.nvim",
+	event = "VeryLazy",
+	build = ":UpdateRemotePlugins",
+	config = function()
+		local wilder = require("wilder")
+		wilder.setup({
+			modes = { ":", "/", "?" },
+			next_key = "<C-j>",
+			previous_key = "<C-k>",
+		})
+		wilder.set_option(
+			"renderer",
+			wilder.renderer_mux({
+				[":"] = wilder.popupmenu_renderer({
+					highlighter = wilder.basic_highlighter(),
+				}),
+				["/"] = wilder.wildmenu_renderer({
+					highlighter = wilder.basic_highlighter(),
+				}),
+			})
+		)
 
-    wilder.set_option(
-      "renderer",
-      wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-        highlights = {
-          border = "Normal", -- highlight to use for the border
-        },
-        -- 'single', 'double', 'rounded' or 'solid'
-        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-        border = "rounded",
-      }))
-    )
-  end,
+		wilder.set_option(
+			"renderer",
+			wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+				highlights = {
+					border = "Normal", -- highlight to use for the border
+				},
+				-- 'single', 'double', 'rounded' or 'solid'
+				-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+				border = "rounded",
+			}))
+		)
+	end,
 }
 table.insert(M, spec)
 return M
