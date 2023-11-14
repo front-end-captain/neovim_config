1b8559babbb77cef605775dc1c178f05a0d0675c
front-end-captain <18810940932@163.com>
2023-11-14 10:27:56 +0800

    feat: add keymao to esc terminal insert mode

--- lua/plugins/_toggleterm.lua
+++ lua/plugins/_toggleterm.lua
@@ -27,4 +27,13 @@ vim.api.nvim_set_keymap(
   ":ToggleTerm direction=vertical<CR>",
   { noremap = true, silent = true }
 )
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+
 return M
