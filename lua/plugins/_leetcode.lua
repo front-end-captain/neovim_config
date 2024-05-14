e869bd2daae20c6516f49719081dfbb7c2ec8de2
front-end-captain <18810940932@163.com>
2024-05-14 12:06:00 +0800

    feat: leetcode

---
+++ lua/plugins/_leetcode.lua
@@ -0,0 +1,29 @@
+local M = {}
+
+local spec = {
+  "kawre/leetcode.nvim",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    lang = "javascript",
+    cn = {
+      enabled = true,
+      translator = true,
+      translate_problems = true,
+    },
+  },
+}
+table.insert(M, spec)
+
+vim.api.nvim_set_keymap("n", "<leader>r", ":Leet run<CR>", { noremap = true, silent = true })
+
+return M
