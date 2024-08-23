ef2f85c65cda467750b34e82101f4d5a35170df3
front-end-captain <18810940932@163.com>
2024-08-23 19:24:45 +0800

    feat: disable virtcolumn

--- lua/plugins/virtcolumn.lua
+++ lua/plugins/virtcolumn.lua
@@ -1,13 +1,15 @@
 local M = {}
 
-vim.wo.signcolumn = "yes"
-vim.opt.colorcolumn = "100,120"
-vim.g.virtcolumn_char = "'"
--- vim.g.virtcolumn_char = '▕'
-vim.g.virtcolumn_priority = 10
-
 local spec = {
   "xiyaowong/virtcolumn.nvim",
+  enabled = false,
+  init = function()
+    vim.wo.signcolumn = "yes"
+    vim.opt.colorcolumn = "100,120"
+    vim.g.virtcolumn_char = "'"
+    -- vim.g.virtcolumn_char = '▕'
+    vim.g.virtcolumn_priority = 10
+  end,
 }
 table.insert(M, spec)
 
