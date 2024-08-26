52fa38d7b6e8d8ec80e31461b875cfea073f4b1a
front-end-captain <18810940932@163.com>
2024-08-26 12:41:04 +0800

    feat: update language-server key maps

--- lua/plugins/language-server.lua
+++ lua/plugins/language-server.lua
@@ -64,6 +64,10 @@ local spec = {
   config = function()
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
     local lsp_signature = require("lsp_signature")
+    local telescope_builtin_pickers = require("telescope.builtin")
+    local telescope_builtin_picker_opts = {
+      initial_mode = "normal",
+    }
 
     local on_attach = function(_, bufnr)
       local nmap = function(keys, func, desc)
@@ -75,25 +79,22 @@ local spec = {
       end
 
       nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-      nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+      nmap("gd", function()
+        telescope_builtin_pickers.lsp_definitions(telescope_builtin_picker_opts)
+      end, "[G]oto [D]efinition")
+      nmap("gi", function()
+        telescope_builtin_pickers.lsp_implementations(telescope_builtin_picker_opts)
+      end, "[G]oto [I]mplementation")
+      nmap("gr", function()
+        telescope_builtin_pickers.lsp_references(telescope_builtin_picker_opts)
+      end, "[G]oto [R]eferences")
       nmap("gh", "<cmd>Lspsaga hover_doc<CR>", "Hover Documentation")
-      nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-      nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-      nmap("<gH>", vim.lsp.buf.signature_help, "Signature Documentation")
-      nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-      nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-      nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, "[W]orkspace [L]ist Folders")
-      nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+
       nmap("<leader>rn", "<cmd>Lspsaga rename ++project<cr>", "[R]e[n]ame")
       nmap("<leader>ca", "<cmd>Lspsaga code_action<CR>", "[C]ode [A]ction")
-      nmap("<leader>O", "<cmd>Lspsaga outline<CR>", "[O]utline toggle")
       nmap("<leader>da", vim.diagnostic.open_float, "[D]i[A]gnostics")
-      -- nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
-      -- nmap("<leader>f", function()
-      --   vim.lsp.buf.format { async = true }
-      -- end, "[F]ormat code")
+      nmap("<leader>O", "<cmd>Lspsaga outline<CR>", "[O]utline toggle")
+
       lsp_signature.on_attach({
         bind = true,
         handler_opts = {
